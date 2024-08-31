caa43e9827f5207c6c8368e49aa1ff203aa3b9c0
Vivek Rawat <vivekrawat0107@gmail.com>
2024-09-01 00:59:36 +0530

    Added nav routs

---
+++ src/app/services/page.jsx
@@ -0,0 +1,11 @@
+
+
+const Services = () => {
+  return (
+      <div>
+          services
+    </div>
+  )
+}
+
+export default Services;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,21 +6,19 @@ import Nav from "./Nav";
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-centre ">
         <Link href="/">
           <h1 className="text-4xl font-semibold">
             Vivek<span className="text-accent">.</span>
           </h1>
         </Link>
-        <div className="hidden xl:flex items-center gap">
-                  <Nav />
-                  <Link href="/contact">
-                      <Button>
-                          Hire Me
-                      </Button>
-                  </Link>
-              </div>
-              <div className="xl:hidden">Mobile Nav</div>
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire Me</Button>
+          </Link>
+        </div>
+        <div className="xl:hidden">Mobile Nav</div>
       </div>
     </header>
   );
