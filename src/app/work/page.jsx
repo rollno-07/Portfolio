6e910f846fbea45e59081881db67f875c0d5e5b0
Vivek Rawat <vivekrawat0107@gmail.com>
2024-09-03 02:52:09 +0530

    bug fix

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -101,7 +101,7 @@ const Work = () => {
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
-          <div className="w-full xl:w-[50%] xl:h-[460px] flex felx-col xl:justify-between order-2 xl:order-none">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
             <div className="flex flex-col gap-[30px] h-[50%]">
               <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
                 {project.num}
