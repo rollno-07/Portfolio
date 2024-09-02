b1b840365460e0af02ce528e8bcd2ac6dd773b77
Vivek Rawat <vivekrawat0107@gmail.com>
2024-09-03 01:50:46 +0530

    Final

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,27 +1,27 @@
-import Link from "next/link"
+import Link from "next/link";
 
-import {FaGithub,FaLinkedinIn,FaTwitter} from "react-icons/fa"
+import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 const socials = [
-  { icon: <FaGithub />, path: "" },
-  { icon: <FaLinkedinIn />, path: "" },
-  { icon: <FaTwitter />, path: "" },
+  { icon: <FaGithub />, path: "https://github.com/rollno-07" },
+  {
+    icon: <FaLinkedinIn />,
+    path: "https://www.linkedin.com/in/vivek-kumar-rawat-04a64b102/",
+  },
 ];
 
-const Social = ({containerStyles, iconStyles}) => {
+const Social = ({ containerStyles, iconStyles }) => {
   return (
-      <div className={containerStyles}>
-          {
-              socials.map((item, index) => {
-                  return(
-                      <Link key={index} href={item.path} className={iconStyles}>
-                      {item.icon}
-                      </Link>
-                 ) 
-              })
-      }  
-      </div>
-  )
-}
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Social
+export default Social;
